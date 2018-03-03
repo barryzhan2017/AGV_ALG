89f18b87e8e7b1fa2566b7dca44b79ea9b6c755e
zhanzhicheng <554813245@qq.com>
2018-03-03 12:05:43 +0800

    增加了遗传算法中的路径搜索部分

--- src/main/java/org/spring/springboot/controller/GeneticAlogrithmControll.java
+++ src/main/java/org/spring/springboot/controller/GeneticAlogrithmControll.java
@@ -1,14 +1,34 @@
 package org.spring.springboot.controller;
 
+import org.spring.springboot.Algorithm.GeneticAlgorithm.FeasiblePathGrowth;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.ujmp.core.Matrix;
 
 @RestController
 public class GeneticAlogrithmControll {
 
     @RequestMapping(value = "/api/genetic", method = RequestMethod.GET)
     public String findOneCity() {
+        int[] priorityChromosome = {1,9,7,4,5};
+        FeasiblePathGrowth feasiblePathGrowth = new FeasiblePathGrowth();
+        Matrix testGraph = Matrix.Factory.ones(5,5);
+        testGraph.setAsInt(0,0,0);
+        testGraph.setAsInt(0,1,1);
+        testGraph.setAsInt(0,2,2);
+        testGraph.setAsInt(0,3,3);
+        testGraph.setAsInt(0,4,4);
+        testGraph.setAsInt(0,0,3);
+        testGraph.setAsInt(0,1,4);
+        testGraph.setAsInt(0,2,4);
+        testGraph.setAsInt(0,3,0);
+        testGraph.setAsInt(0,4,1);
+        testGraph.setAsInt(0,4,2);
+    //    testGraph.setAsInt(0,4,3);
+    //    testGraph.setAsInt(0,3,4);
+
+        feasiblePathGrowth.feasiblePathGrowth(testGraph,0,3,priorityChromosome);
         return "hello";
     }
 }
