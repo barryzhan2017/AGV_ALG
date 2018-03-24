1e4b9d3e40259747536295fa7cf4f37c3e60e7eb
zhanzhicheng <554813245@qq.com>
2018-03-25 00:23:28 +0800

    前后端接成功

---
+++ src/main/java/org/spring/springboot/interceptor/ProcessInterceptor.java
@@ -0,0 +1,43 @@
+package org.spring.springboot.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Created by xl on 2018/1/2.
+ */
+//解决axios跨域请求 http://blog.csdn.net/brave_coder/article/details/77103899
+public class ProcessInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("aaa");
+
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+
+        httpServletResponse.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
+        httpServletResponse.setHeader("X-Powered-By","Jetty");
+
+
+        String method= httpServletRequest.getMethod();
+        System.out.println("bbb");
+        if (method.equals("OPTIONS")){
+            httpServletResponse.setStatus(200);
+            return false;
+        }
+
+//        System.out.println(method);
+        System.out.println("aaa");
+        return true;
+    }
+
+
+}
