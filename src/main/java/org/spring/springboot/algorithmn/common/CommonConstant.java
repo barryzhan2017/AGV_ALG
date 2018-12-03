373235457478deb16c62cfa0620776cab9ada5c3
zhanzh <554813245@qq.com>
2018-12-03 11:18:29 +0800

    Complete part of routing

---
+++ src/main/java/org/spring/springboot/algorithmn/common/CommonConstant.java
@@ -0,0 +1,10 @@
+package org.spring.springboot.algorithmn.common;
+
+public class CommonConstant {
+    public static final int MAX_EDGE = 999999;
+    public static final int INFINITE = 999999;
+    //The distance for a AGV to cross the crossing to the other road.
+    public static final double CROSSING_DISTANCE = 4;
+
+
+}
--- src/main/java/org/spring/springboot/algorithmn/conflict_free_routing/Routing.java
+++ src/main/java/org/spring/springboot/algorithmn/conflict_free_routing/Routing.java
@@ -1,5 +1,7 @@
 package org.spring.springboot.algorithmn.conflict_free_routing;
 
+import org.spring.springboot.algorithmn.common.CommonConstant;
+
 import java.util.List;
 
 //This is the algorithm to calculate the conflict-free route for one AGV given the current time windows.
@@ -14,6 +16,39 @@ public class Routing {
     private double[][] graph;
     //Current time window the AGV occupies
     private TimeWindow currentTimeWindow;
+    private double AGVLength;
     //consider the buffer information. how to get
+
+
+    //Get the route of the AGV by applying conflict-free routing algorithm
+    public List<Integer> route() {
+
+    }
+
+    //Test if one time window is reachable for the other one and return the path and minimum time required to travel
+    private double testReachability(int endNode, TimeWindow currentTimeWindow, Integer[] path, double speed) {
+        int startNode = currentTimeWindow.getNodeNumber();
+        //Check for path availability
+        if (graph[startNode][endNode] == CommonConstant.MAX_EDGE) {
+            path[0] = -1;
+            return CommonConstant.INFINITE;
+        }
+        //Check for time availability. If the free time window is long enough for the AGV to pass the crossing
+        double distance = graph[startNode][endNode];
+        //The time for the AGV to reach the entrance edge of crossing
+        double timeToReachCrossing =  (distance - AGVLength)/speed;
+        //Search for the nearest available time window
+        for (TimeWindow possibleTimeWindow : freeTimeWindowList) {
+            if (possibleTimeWindow.getNodeNumber() == endNode
+                    && possibleTimeWindow.getStartTime() > currentTimeWindow.getEndTime()) {
+                //Find the nearest possible time to enter the crossing
+                double validTimeToCrossEndNode = Math.max(possibleTimeWindow.getStartTime(), timeToReachCrossing);
+                if (validTimeToCrossEndNode > possibleTimeWindow.getEndTime()) {
+
+                }
+            }
+        }
+    }
+
 
 }
