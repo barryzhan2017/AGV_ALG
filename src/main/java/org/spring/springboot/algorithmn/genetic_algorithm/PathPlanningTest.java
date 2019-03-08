5e842f448f63cdbd2e146da7e6fcdeabb2b870b5
zhanzhicheng <554813245@qq.com>
2019-03-08 19:37:24 +0800

    Finish implementation and basic test for path planing

--- src/main/java/org/spring/springboot/algorithmn/genetic_algorithm/PathPlanningTest.java
+++ src/main/java/org/spring/springboot/algorithmn/genetic_algorithm/PathPlanningTest.java
@@ -52,4 +52,66 @@ public class PathPlanningTest {
         assertEquals((CommonConstant.BUFFER_PATH_LENGTH + 8 + 2 * CommonConstant.CROSSING_DISTANCE)/ CommonTestConstant.AGV_SPEED, fitness[0], 0.000001);
         assertEquals((CommonConstant.BUFFER_PATH_LENGTH + 8 + 2 * CommonConstant.CROSSING_DISTANCE)/ CommonTestConstant.AGV_SPEED, time[0], 0.000001);
     }
+
+    //Start from node 8, AGV wants to go node 8. Path should form as 8-8. Time and fitness should remain the same.
+    @Test
+    public void shouldPathCreatedCorrectlyWhenAGVGoesToTheCurrentPosition() {
+        List<List<Integer>> bufferSet = CommonTestConstant.getBufferForTestGraph2();
+        int startNode = 7;
+        int endNode = 7;
+        double[] fitness = {0};
+        double[] time = {0};
+        PathPlanning pathPlanning = new PathPlanning(1, penalty, CommonTestConstant.AGV_SPEED, CommonConstant.BUFFER_PATH_LENGTH);
+        Routing routing = new Routing(graph, bufferSet, CommonTestConstant.AGV_SPEED, 10);
+        List<Path> paths = pathPlanning.getPath(routing, endNode, startNode, 0, fitness, time);
+        assertEquals(1, paths.size());
+        Path path1 = paths.get(0);
+        assertEquals(7, path1.getStartNode());
+        assertEquals(7, path1.getEndNode());
+        assertEquals(0, path1.getTime(), 0.000001);
+        assertFalse(path1.isLoop());
+        assertEquals(0, fitness[0], 0.000001);
+        assertEquals(0, time[0], 0.000001);
+    }
+
+    //One previous AGV 0 goes as 206--8--7. The current AGV 1 goes from 7 to 8.
+    //It should adjust the route as 7--6--9--8 to avoid conflict. Time and fitness should be changed correspondingly.
+    @Test
+    public void shouldGetPathCorrectlyFromRoutingWhenThereIsOnePreviousAGVGoingFrom206To8To7() {
+        List<List<Integer>> bufferSet = CommonTestConstant.getBufferForTestGraph2();
+        int startNode = 205;
+        int endNode = 6;
+        double[] fitness = {0, 0};
+        double[] time = {0, 0};
+        PathPlanning pathPlanning = new PathPlanning(1, penalty, CommonTestConstant.AGV_SPEED, CommonConstant.BUFFER_PATH_LENGTH);
+        Routing routing = new Routing(graph, bufferSet, CommonTestConstant.AGV_SPEED, 10);
+        pathPlanning.getPath(routing, endNode, startNode, 0, fitness, time);
+        int currentStartNode = 6;
+        int currentEndNode = 7;
+        List<Path> paths = pathPlanning.getPath(routing, currentEndNode, currentStartNode, 1, fitness, time);
+        assertEquals(3, paths.size());
+        Path path1 = paths.get(0);
+        assertEquals(6, path1.getStartNode());
+        assertEquals(5, path1.getEndNode());
+        assertEquals(10 / CommonTestConstant.AGV_SPEED, path1.getTime(), 0.000001);
+        assertFalse(path1.isLoop());
+        Path path2 = paths.get(1);
+        assertEquals(5, path2.getStartNode());
+        assertEquals(8, path2.getEndNode());
+        assertEquals(8 / CommonTestConstant.AGV_SPEED, path2.getTime(), 0.000001);
+        assertFalse(path2.isLoop());
+        Path path3 = paths.get(2);
+        assertEquals(8, path3.getStartNode());
+        assertEquals(7, path3.getEndNode());
+        assertEquals(10 / CommonTestConstant.AGV_SPEED, path3.getTime(), 0.000001);
+        assertFalse(path3.isLoop());
+        assertEquals((10 * 2 + 8 + 3 * CommonConstant.CROSSING_DISTANCE)/ CommonTestConstant.AGV_SPEED, fitness[1], 0.000001);
+        assertEquals((10 * 2 + 8 + 3 * CommonConstant.CROSSING_DISTANCE)/ CommonTestConstant.AGV_SPEED, time[1], 0.000001);
+    }
+
+//    //
+//    @Test
+//    public void shouldTwoAGVsAdjustTheirPositionsIn3SpaceBufferWhenOneAGVsGoesOutOfTheBuffer() {
+//
+//    }
 }
